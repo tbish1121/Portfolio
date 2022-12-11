ce54ccd52f88d83a908470db69881a9610da0520
Taylor <tbish1121@gmail.com>
2022-12-10 22:39:17 -0500

    Updates

---
+++ precache-manifest.8e055e0f8d8498ce4cab450e6ff71298.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "74f11acb2e436a62aa35",
+    "url": "/Portfolio/static/css/main.7fc1a616.chunk.css"
+  },
+  {
+    "revision": "74f11acb2e436a62aa35",
+    "url": "/Portfolio/static/js/main.74f11acb.chunk.js"
+  },
+  {
+    "revision": "6c6fe19a24bcafd10966",
+    "url": "/Portfolio/static/css/1.790ac690.chunk.css"
+  },
+  {
+    "revision": "6c6fe19a24bcafd10966",
+    "url": "/Portfolio/static/js/1.6c6fe19a.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "593886ced7da9ecbfed3fbde0c7c0b29",
+    "url": "/Portfolio/index.html"
+  }
+];
